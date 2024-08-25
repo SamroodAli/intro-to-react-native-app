31c06f2b89be4342cc355dbbb2243b2d79a25416
SamroodAli <samrood@peoplegrove.com>
2024-08-25 16:49:56 +0400

    batman: expo app with tailwind and linting

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-white flex-1 justify-center">
+      <View className="border-b-primary-light border-b px-2 py-4">
+        <Text className="font-extralight text-lg">Coffee world</Text>
+      </View>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#1a75df'
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
