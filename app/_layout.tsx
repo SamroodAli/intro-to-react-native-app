5dae646113a335786725d4ff53a01c5df167e2c5
SamroodAli <samrood@peoplegrove.com>
2024-08-26 20:47:16 +0400

    feat: add icons for bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,41 @@
 import { Tabs } from 'expo-router';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { THEME } from '@/assets/theme';
 
 export default function RootLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Shopping List' }} />
-      <Tabs.Screen name="counter" options={{ title: 'Counter' }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: THEME.colorPrimary,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Shopping List',
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: 'Counter',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" color={color} size={size} />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="idea"
         options={{
           title: 'Idea',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
