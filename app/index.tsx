ad69044a526bc7f2368b1e62d37b4b66066d7542
SamroodAli <samrood@peoplegrove.com>
2024-09-24 10:15:56 +0530

    feat: add dynamic shoppling list items

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,67 @@
-import { View } from 'react-native';
+import { TextInput, View } from 'react-native';
 import { ShoppingListItem } from '../components/ShoppingListItem';
+import { useState } from 'react';
+
+type ListItem = {
+  id: string;
+  name: string;
+  isCompleted: boolean;
+};
+
+const initialList: ListItem[] = [
+  {
+    id: '1',
+    name: 'Coffee',
+    isCompleted: false,
+  },
+  {
+    id: '2',
+    name: 'Tea',
+    isCompleted: false,
+  },
+  {
+    id: '3',
+    name: 'Sugar',
+    isCompleted: false,
+  },
+];
 
 export default function App() {
+  const [value, setValue] = useState('');
+  const [shoppingList, setShoppingList] = useState<ListItem[]>(initialList);
+
+  const onHandleSubmit = () => {
+    if (value) {
+      setShoppingList((prevList) => [
+        {
+          id: new Date().toISOString(),
+          name: value,
+          isCompleted: false,
+        },
+        ...prevList,
+      ]);
+      setValue('');
+    }
+  };
+
   return (
-    <View className="flex-1 justify-center">
-      <ShoppingListItem name="Coffee" isCompleted />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" />
+    <View className="flex-1 pt-4">
+      <TextInput
+        className="border-gray-200 border-2 p-3 mx-3 mb-3 text-lg rounded-full"
+        value={value}
+        onChangeText={setValue}
+        placeholder="E.g Coffee"
+        // what the 'return' key in keyboard look like
+        returnKeyType="done"
+        onSubmitEditing={onHandleSubmit}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem
+          key={item.id}
+          name={item.name}
+          isCompleted={item.isCompleted}
+        />
+      ))}
     </View>
   );
 }
